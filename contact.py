16819a045a22af1675fea4776960e83e8a379249
katghe <katghe@gmail.com>
2020-02-16 22:26:06 +0300

    С ошибками, переделать/Добавление контакта

---
+++ contact.py
@@ -0,0 +1,23 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home_phone, mobile_phone, work_phone, main_email, bday, bmonth, byear, aday, amonth, ayear, address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.main_email = main_email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
+
