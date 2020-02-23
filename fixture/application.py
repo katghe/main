b9637e86f8c54f1e6857acd49601629300802a54
Kate <katghe@gmail.com>
2020-02-23 22:58:47 +0300

    Добавлены помощники. Задание 4+5

--- fixture/application.py
+++ fixture/application.py
@@ -2,14 +2,21 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.contact import ContactHelper
+from fixture.group import GroupHelper
+
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
 
     def destroy(self):
         self.wd.quit()
--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -9,14 +9,21 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
-    
+
 def test_add_new_contact(app):
     app.session.login(login_name="admin", password="secret")
-    app.contact.create(Contact(firstname="Kate", middlename="Kat2", lastname="Yoffe", nickname="katghe", title="new user", company="addr", address="Saint - Petersburg", home_phone="888888", mobile_phone="89600765656", work_phone="99999", main_email="email@am.com", bday="16", bmonth="October", byear="1992", aday="16", amonth="December", ayear="1990", address2="dfedgafgfda", phone2="dafdfafggag", notes="123455677"))
+    app.contact.create(
+        Contact(firstname="Kate", middlename="Kat2", lastname="Yoffe", nickname="katghe", title="new user",
+                company="addr", address="Saint - Petersburg", home_phone="888888", mobile_phone="89600765656",
+                work_phone="99999", main_email="email@am.com", bday="16", bmonth="October", byear="1992", aday="16",
+                amonth="December", ayear="1990", address2="dfedgafgfda", phone2="dafdfafggag", notes="123455677"))
     app.session.logout()
+
 
 def test_add_new_contact_empty(app):
     app.session.login(login_name="admin", password="secret")
-    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home_phone="", mobile_phone="", work_phone="", main_email="", bday="-", bmonth="-", byear="", aday="-", amonth="-", ayear="", address2="", phone2="", notes=""))
+    app.contact.create(
+        Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home_phone="",
+                mobile_phone="", work_phone="", main_email="", bday="-", bmonth="-", byear="", aday="-", amonth="-",
+                ayear="", address2="", phone2="", notes=""))
     app.session.logout()
-
