8cab26940440c5bca40077bc32333ff2681d7c4f
katghe <katghe@gmail.com>
2020-03-17 11:07:16 +0300

    Задание 13. Немного изменен метод редактирования контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -30,7 +30,6 @@ class ContactHelper:
     def edit_contact_by_index(self, index, new_contact_data):
         wd = self.app.wd
         self.app.open_home_page()
-        self.select_contact_by_index(index)
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         self.fill_contact_form(new_contact_data)
         wd.find_element_by_name("update").click()
