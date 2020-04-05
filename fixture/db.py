363ffe3075bc013bdb5c8af7bdf614ab0b48a85b
Kate <katghe@gmail.com>
2020-04-05 15:36:11 +0300

    Задание 21. Добавлен тест для проверки инфо с главной страницы с данными из бд

--- fixture/db.py
+++ fixture/db.py
@@ -27,7 +27,7 @@ class DbFixture:
         clist = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook")
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated = '0000-00-00 00:00:00'" )
             for row in cursor:
                 (id, firstname, lastname) = row
                 clist.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
--- test/contact_tests/test_phones.py
+++ test/contact_tests/test_phones.py
@@ -1,5 +1,5 @@
 import re
-import random
+from model.contact import Contact
 
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
@@ -9,6 +9,16 @@ def test_phones_on_home_page(app):
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
 
+
+def test_contact_edit_and_home(app, db):
+    contacts_bd = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    contacts_on_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    for i in range(len(contacts_on_home_page)):
+        contact_from_home_page = contacts_on_home_page[i]
+        contact_from_db = contacts_bd[i]
+        assert contact_from_home_page.id == contact_from_db.id
+        assert contact_from_home_page.firstname == contact_from_db.firstname
+        assert contact_from_home_page.lastname == contact_from_db.lastname
 
 
 
